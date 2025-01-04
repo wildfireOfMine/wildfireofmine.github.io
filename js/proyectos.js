b57b1a5317af7bcce1783d2fcefc44bffdaeacf2
wildfireOfMine <iamtheanguera@gmail.com>
2025-01-04 16:31:56 +0100

    Añadida la funcionalidad de JS a proyectos.js, y modificado proyectos.html

---
+++ js/proyectos.js
@@ -0,0 +1,25 @@
+const usuario = "wildfireOfMine"
+const url = `https://api.github.com/users/${usuario}/repos`;
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const lista = document.getElementById("repositorio");
+    fetch(url)
+    .then(response=>response.json())
+    .then(data =>{
+        data.forEach(element => {
+            console.log(element);
+            console.log(element.html_url);
+            const enlace = document.createElement("a");
+            enlace.href = element.html_url;
+            enlace.target = "_blank";
+            const elemento = document.createElement("li");
+            elemento.innerHTML = element.name;
+            enlace.appendChild(elemento);
+            lista.appendChild(enlace);
+        });
+    })
+    .catch(error =>{
+        console.log(error);
+    });
+});
